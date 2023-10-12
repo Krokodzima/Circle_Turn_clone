db7ba5dc8f5a44cb2ccea5a348bca8e97c390070
Krokodzima <143348829+Krokodzima@users.noreply.github.com>
2023-10-12 11:16:02 +0300

    v 0.0.4
    added
    saving Score in a wallet
    display score on the MainScene

--- Circle_Turn_clone/Assets/Scripts/MainMenu.cs
+++ Circle_Turn_clone/Assets/Scripts/MainMenu.cs
@@ -2,9 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; // библиотека менеджера сцен
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public int money;
+    public int earnedMoney;
+    public Text moneyText;
+
+    private void Start()
+    {
+        // накопление очков после каждой игры
+        money = PlayerPrefs.GetInt("Money");
+        earnedMoney = PlayerPrefs.GetInt("Score");
+        money += earnedMoney;
+        PlayerPrefs.SetInt("Money", money);
+        moneyText.text = money.ToString();
+
+        // фикс бага - получение денег при каждом заходе в игру
+        earnedMoney = 0;
+        PlayerPrefs.SetInt("Score", 0);
+    }
+
     public void StartGame() 
     {
         SceneManager.LoadScene(1); // (0) - MainMenu, (1) - PlayScene, (2) - Lose
